dc2dd628cccbc42bc071627ee33aa2a508482f7a
o.rzazade <o.rzazade@gmail.com>
2019-10-10 21:39:26 +0300

    #2 favorites page added

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,7 +3,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { Grid, Tooltip, Link, IconButton } from "@material-ui/core";
+import { Grid, Tooltip, Link, IconButton, Icon, ButtonBase } from "@material-ui/core";
 import { Context } from "contexts/theme-context";
 import { LightIcon, GithubIcon, AboutIcon } from "components/svg-icons";
 import history from "browser-history";
@@ -21,20 +21,32 @@ export const Header = () => {
       <AppBar>
         <Grid container>
           <Grid item xs={10}>
-            <Toolbar>
-              <Typography
-                onClick={() =>
-                  history.push({
-                    pathname: "/"
-                  })
-                }
-                variant="h5"
-              >
-                Pubgheatmap.net
-              </Typography>
-            </Toolbar>
+            <ButtonBase
+              onClick={() =>
+                history.push({
+                  pathname: "/"
+                })
+              }
+            >
+              <Toolbar>
+                <Typography variant="h5">Pubgheatmap.net</Typography>
+              </Toolbar>
+            </ButtonBase>
           </Grid>
           <Grid item container spacing={2} justify="space-around" alignItems="center" xs={2}>
+            <Tooltip interactive title="Go to favorites page">
+              <IconButton
+                style={{ color: "#fff" }}
+                edge="start"
+                onClick={() =>
+                  history.push({
+                    pathname: "/favorites"
+                  })
+                }
+              >
+                <Icon color="">star</Icon>
+              </IconButton>
+            </Tooltip>
             <Tooltip interactive title="Toggle light/dark theme">
               <IconButton onClick={handleChange}>
                 <LightIcon isDarkMode={isDarkMode} />
---
+++ src/views/favorites/favorites.style.js
@@ -0,0 +1,30 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%",
+    height: "calc(100vh - 64px)",
+    borderRadius: "0px"
+  },
+  paper: {
+    width: "100%",
+    marginBottom: theme.spacing(2)
+  },
+  table: {
+    minWidth: 750
+  },
+  tableWrapper: {
+    overflowX: "auto"
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: "rect(0 0 0 0)",
+    height: 1,
+    margin: -1,
+    overflow: "hidden",
+    padding: 0,
+    position: "absolute",
+    top: 20,
+    width: 1
+  }
+}));
